0139a4df7e87a4c7204a4815d4624a779e380355
Kawser <Sharif.al.kawser11@gmail.com>
2024-02-19 23:59:58 +0600

    ui file

---
+++ lib/hadith_details_page/hadisDetalis.dart
@@ -0,0 +1,33 @@
+import 'package:al_hadith_clone/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class HadithDetalis extends StatelessWidget {
+  final String item2;
+
+  const HadithDetalis({super.key, required this.item2});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: AppColors.primaryColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppBar(
+              backgroundColor: AppColors.primaryColor,
+              title: Text("bukhari"),
+            ),
+            Container(
+              child: Column(
+                children: [
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
