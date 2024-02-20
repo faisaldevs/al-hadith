fbfae6524d3b104409fda0b007a780aecdf0a5db
Kawser <Sharif.al.kawser11@gmail.com>
2024-02-20 22:48:32 +0600

    drawer install

--- lib/app/screens/home_page/home_page.dart
+++ lib/app/screens/home_page/home_page.dart
@@ -58,7 +58,7 @@ class HomePage extends StatelessWidget {
               ),
 
               // -----------slider------------
-
+              MyImageSlider(texts: texts),
 
 
               // -----------slider------------
