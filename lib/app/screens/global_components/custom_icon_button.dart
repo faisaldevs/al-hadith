3bdcb29c92aa2ac0635e42508612afd6b1d595c6
Md Faisal Ahamed <faisalah331@gmail.com>
2024-02-22 00:11:33 +0600

    Animated Action Button

--- lib/app/screens/global_components/custom_icon_button.dart
+++ lib/app/screens/global_components/custom_icon_button.dart
@@ -2,19 +2,35 @@ import 'package:flutter/material.dart';
 
 class CustomIconButtons extends StatelessWidget {
   const CustomIconButtons({super.key, required this.imageUrl});
+
   final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(left: 10),
-      padding: const EdgeInsets.all(15),
+      // padding: const EdgeInsets.all(15),
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
-      child: Image.asset(imageUrl,width: 30,),
-
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: const MaterialStatePropertyAll(EdgeInsets.all(15)),
+          // backgroundColor: MaterialStatePropertyAll(Colors.white),
+          // overlayColor: MaterialStatePropertyAll(Colors.white),
+          shape: MaterialStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        child: Image.asset(
+          imageUrl,
+          width: 30,
+        ),
+        onPressed: () {},
+      ),
     );
   }
 }
