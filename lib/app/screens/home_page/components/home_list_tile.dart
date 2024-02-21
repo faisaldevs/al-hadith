67bdc605b1c288cff1745525c4256144090c3e3f
Md Faisal Ahamed <faisalah331@gmail.com>
2024-02-21 22:52:14 +0600

    click able listview tile

--- lib/app/screens/home_page/components/home_list_tile.dart
+++ lib/app/screens/home_page/components/home_list_tile.dart
@@ -7,71 +7,78 @@ class HomeListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: (){},
-      child: Container(
-        // margin: const EdgeInsets.symmetric(horizontal: 16),
-        margin: const EdgeInsets.only(bottom: 10, left: 16, right: 16),
-        padding: const EdgeInsets.all(8),
-        width: Get.width * .8, // --------width----------
-        height: 70,
-        decoration: BoxDecoration(
-          color: Colors.blue[100],
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              // width: Get.width * .6,
-              child: Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10, left: 16, right: 16),
+      // padding: const EdgeInsets.all(8),
+      width: Get.width * .8,
+      // --------width----------
+      height: 70,
+      child: ElevatedButton(
+          style: ButtonStyle(
+              // padding: MaterialStatePropertyAll(EdgeInsets.zero),
+              elevation: const MaterialStatePropertyAll(0),
+              backgroundColor: MaterialStatePropertyAll(
+                Colors.blue[100],
+              ),
+              // side: MaterialStatePropertyAll(BorderSide()),
+              shape: MaterialStatePropertyAll(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )),
+          onPressed: () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                // width: Get.width * .6,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
 
-                children: [
-                  Container(
-                    // width: Get.width * .25,
-                    height: Get.height,
-                    margin: const EdgeInsets.only(right: 20),
-                    child: CircleAvatar(
-                      child: Image.asset(ImageLink.listTile),
+                  children: [
+                    Container(
+                      // width: Get.width * .25,
+                      height: Get.height,
+                      margin: const EdgeInsets.only(right: 20),
+                      child: CircleAvatar(
+                        child: Image.asset(ImageLink.listTile),
+                      ),
                     ),
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "সব হাদিসের",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 18),
+                        ),
+                        Text(
+                          "সব হাদিসের",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal, fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "7542",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                   ),
-                  const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "সব হাদিসের",
-                        style:
-                            TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
-                      ),
-                      Text(
-                        "সব হাদিসের",
-                        style: TextStyle(
-                            fontWeight: FontWeight.normal, fontSize: 14),
-                      ),
-                    ],
+                  Text(
+                    "হাদিস",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                   ),
                 ],
-              ),
-            ),
-            const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "7542",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-                Text(
-                  "হাদিস",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+              )
+            ],
+          )),
     );
   }
 }
